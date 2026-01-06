676613a713d9b41267c9c45376d624206595d9bc
viswa.g <viswa.g@ltts.com>
2026-01-06 15:35:01 +0530

    "Add switch case unit test for ethernet agent"

--- source/test/switch_case_test.c
+++ source/test/switch_case_test.c
@@ -1,24 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int x = 10                    
-    printf("Value: %d\n", x);
-    int y = 3;
+#define ARR_SIZE 5
 
-    char *p = malloc(10);
-    if (p == NULL)
-        return 1;
-    for (int i = 0; i < 10; i++)
-        p[i] = 'A';
+void func2();
+void func3();
+void func4();
+void func5();
 
-    free(p);
-    p[0] = 'B';
 
-    free(p);
+void func1()
+{
+    int *arr1 = (int *)malloc(ARR_SIZE * sizeof(int));
+    for (int i = 0; i < ARR_SIZE; i++)
+        arr1[i] = i;
 
-    int *ptr = NULL;
-    *ptr = 5;
+    func2();
 
+    
+}
+
+
+void func2()
+{
+    int *arr2 = (int *)malloc(ARR_SIZE * sizeof(int));
+    for (int i = 0; i < ARR_SIZE; i++)
+        arr2[i] = i + 10;
+
+    func3();
+
+  
+}
+
+
+void func3()
+{
+    int *arr3 = (int *)malloc(ARR_SIZE * sizeof(int));
+    for (int i = 0; i < ARR_SIZE; i++)
+        arr3[i] = i + 20;
+
+    func4();
+
+ 
+}
+
+
+void func4()
+{
+    int *arr4 = (int *)malloc(ARR_SIZE * sizeof(int));
+    for (int i = 0; i < ARR_SIZE; i++)
+        arr4[i] = i + 30;
+
+    func5();
+
+}
+
+void func5()
+{
+    int *arr5 = (int *)malloc(ARR_SIZE * sizeof(int));
+    for (int i = 0; i < ARR_SIZE; i++)
+        arr5[i] = i + 40;
+
+    for (int i = 0; i < ARR_SIZE; i++)
+        printf("%d ", arr5[i]);
+
+    printf("\n");
+
+    
+}
+
+int main()
+{
+    func1();
     return 0;
 }
+
